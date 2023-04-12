0d556a1f068e315edd716c7fedf1750cb0cd651b
SeregaTheDed <shtanko33333@mail.ru>
2023-04-12 18:33:37 +0300

    Добавьте файлы проекта.

---
+++ StaticMaskingLibrary/StaticMasker.cs
@@ -0,0 +1,10 @@
+﻿namespace StaticMaskingLibrary
+{
+    public class StaticMasker
+    {
+        public string GetScript()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
